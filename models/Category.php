ce14c0fc23890283fa22ab66d31639a714b13a7d
KornjacaRade <aleksandar.trmcic@gmail.com>
2022-06-01 12:48:12 +0200

    category update added

--- models/Category.php
+++ models/Category.php
@@ -94,7 +94,36 @@ class Category{
         return false;
     }
 
+    // Update Category
+    public function update(){
+        // Create query
+        $query = 'UPDATE '.
+            $this->table .'
+            SET
+            name = :name
+            WHERE
+            id = :id';
 
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        // Clean data
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        // Bind data
+        $stmt->bindParam(':name',$this->name);
+        $stmt->bindParam(':id',$this->id);
+
+
+        // Execute query
+        if($stmt->execute()){
+            return true;
+        }
+        // Print error if something goes wrong
+        printf("Error: %s.\n",$stmt->error);
+        return false;
+    }
 
 
 
